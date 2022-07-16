c9e3aed16cc0fa96dbf88e65c2ca85e4547697b8
JaggedJacks <mathematicalguitarhero@live.com>
2022-07-16 15:27:13 -0400

    Database Interface Started

--- ClosedGL/Source.cpp
+++ ClosedGL/Source.cpp
@@ -4,13 +4,13 @@
 #include"imgui_impl_opengl3.h"
 #include <gtest/gtest.h>
 #include <UserInfo.h>
+#include <GraphicalUser.h>
 
-#define SCREEN_WIDTH 640
-#define SCREEN_HEIGHT 480
 
 using namespace std;
+MultipleUsers userList;
 
-
+/*
 TEST(MissedPayments, CollectionsNotified) {
     MultipleUsers userList;
     EXPECT_EQ(6, userList.user[0].underpaymentCount);
@@ -51,70 +51,196 @@ TEST(TimelyPayments, ExtraPaymentsMade) {
 TEST(UnauthorizedUser, InformationDenied) {
     MultipleUsers userList;
     EXPECT_EQ(userList.user[5].status, "Authentication failed!");
+}*/
+
+TEST(GraphPoints, GraphPointsTranslated) {
+    MultipleUsers userList;
+    cout <<"Printout: " << userList.user[0].lengthOfMembership << "\n";
+    for (int i = 0; i < userList.user[0].lengthOfMembership; i++) {
+        cout << userList.user[0].actualPaymentGraphPoints[i][0]<<",";
+        cout << userList.user[0].actualPaymentGraphPoints[i][1] << ",";
+        cout << userList.user[0].actualPaymentGraphPoints[i][2] << "\n";
+    }
 }
 
-void drawLines(bool revAlert) {
-    glBegin(GL_LINES);
-    if (revAlert) {
-        //Customer 1's info
+GLfloat pointVertex[] = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+GLfloat pointVertex2[] = { SCREEN_WIDTH * 0.75, SCREEN_HEIGHT * 0.75 };
+
+GLfloat lineVertices[] = {//what do the 3 points mean?
+      300,300,
+      310,310,
+      320, 300,
+     330,310,
+      340,300
+  };
+
+GLfloat lineVerticesUser1[] = {//what do the 3 points mean?
+      -.75,-.75,0,
+      -.65,0 ,0,
+      -.55 , -.75 , 0,
+     -.45 ,0 ,0,
+      -.35 ,-.75 ,0
+};
+
+GLfloat lineVerticesUser2[] = {
+    //declare a vertice array of payment length
+    //May have to convert DB info to line vertices
+    -.75,.5,0,
+    -.65,.6 ,0,
+    -.55 , .7 , 0,
+   -.45 ,.7 ,0,
+    -.35 ,.7 ,0
+};
+
+GLfloat lineVerticesUser3[3][9] = { //now you can dynamically declare the line vertices and specify each vertex by the d/b info
+    //declare a vertice array of payment length
+    //May have to convert DB info to line vertices
+    -.75,.5,0,
+    -.65,.6 ,0,
+    -.55 , .7 , 0,
+   -.45 ,.7 ,0,
+   .75,.5,0,
+       .65,.6 ,0,
+    .55 , .7 , 0,
+   .45 ,.7 ,0,
+    -.35 ,.7 ,0
+};
+
+
+void drawVertices(bool revAlert) {
+ 
+
+    if (revAlert) {    
+        glLineWidth(3); //set the line width only for plotted points
+        glEnableClientState(GL_VERTEX_ARRAY);
+
+        const GLvoid* pointer = userList.user[0].actualPaymentGraphPoints;
+
+        /*
+        for (int i = 0; i < userList.user[0].lengthOfMembership; i++) {
+            cout << userList.user[0].actualPaymentGraphPoints[i][0] << ",";
+        }
+        cout<< "\n";
+        for (int i = 0; i < userList.user[0].lengthOfMembership; i++) {
+            cout << userList.user[0].actualPaymentGraphPoints[i][1] << ",";
+        }
+        cout << "\n";
+        for (int i = 0; i < userList.user[0].lengthOfMembership; i++) {
+            cout << userList.user[0].actualPaymentGraphPoints[i][2] << ",";
+        }
+        cout << "\n";
+        */
+        
+        
+        GLfloat lineVerticesUser[36];
+        int k = 0;
+        for (int i = 0; i < userList.user[0].lengthOfMembership; i++) {
+            for (int j = 0;j<3; j++) {
+                //cout << userList.user[0].actualPaymentGraphPoints[i][2] << ",";
+                lineVerticesUser[k] = userList.user[0].actualPaymentGraphPoints[i][j];
+               cout << lineVerticesUser[k] <<",";
+                k++;
+            }
+            cout << "\n";
+        }
+        cout << "\n";
+
+        glVertexPointer(3, GL_FLOAT, 0, lineVerticesUser); //use method to recreate
         glColor3f(0.0, 0.0, 1.0); //blue
-        glVertex2f(-.5, 0);
-        glVertex2f(0, .5);
+        glDrawArrays(GL_LINE_STRIP, 0, userList.user[0].lengthOfMembership);//plots the first x number of points
+        glColor3f(1.0, 1.0, 1.0); //white
+        glDisableClientState(GL_VERTEX_ARRAY);
 
-        //Customer 2's info
-        glColor3f(1.0, 0.0, 0.0); //red
-        glVertex2f(-.5, 0);
-        glVertex2f(0, .3);
-
-        //Customer 3's info
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glVertexPointer(3, GL_FLOAT, 0, lineVerticesUser3);
         glColor3f(0.0, 1.0, 0.0); //green
-        glVertex2f(-.5, 0);
-        glVertex2f(-.1, .5);
+        glDrawArrays(GL_LINE_STRIP, 0, 5);
+        glColor3f(1.0, 1.0, 1.0); //white
+        glDisableClientState(GL_VERTEX_ARRAY);
+        glLineWidth(1); //revert the line width
 
-        RUN_ALL_TESTS();
+        glLineWidth(3); //set the line width only for plotted points
+        glEnableClientState(GL_VERTEX_ARRAY);
+        glVertexPointer(3, GL_FLOAT, 0, lineVerticesUser1); //use method to recreate
+        glColor3f(1.0, 0.0, 0.0); //red
+        glDrawArrays(GL_LINE_STRIP, 0, 5);
+        glColor3f(1.0, 1.0, 1.0); //white
+        glDisableClientState(GL_VERTEX_ARRAY);
+        //RUN_ALL_TESTS();
     }
-        glLineWidth(1);
-        glEnd();
-    
+       //glEnd();
     return;
 }
 
 void createGraph(bool authenticated, bool problemPoint) {
     if (authenticated) {
-
-
-    //old line
-        /*glPushAttrib(GL_ENABLE_BIT);
-        glLineStipple(1, 0xAAAA);
-        glEnable(GL_LINE_STIPPLE);
+        //x axis
         glBegin(GL_LINES);
-        glVertex3f(-.5, .5, -.5);
-        glVertex3f(.5, .5, -.5);*/
+        glVertex2f(START_X, START_Y);
+        glVertex2f(START_X, END_Y);
+        glLineWidth(1);
+        glEnd();
 
-        //new line
-    glColor3f(1.0, 1.0, 1.0);
-    glBegin(GL_LINES);
-    glVertex2f(-.5, 0);
-    glVertex2f(0, 0);
-    glLineWidth(1);
-    glEnd();
+        //y axis
+        glBegin(GL_LINES);
+        glVertex2f(START_X, START_Y);
+        glVertex2f(END_X, START_Y);
+        glLineWidth(1);
+        glEnd();
 
-    glColor3f(1.0, 1.0, 1.0);
-    glBegin(GL_LINES);
-    glVertex2f(-.5, .5);
-    glVertex2f(-.5, 0);
-    glLineWidth(1);
-    glEnd();
-
-    drawLines(problemPoint);
-} //else, clear the screen
+    drawVertices(problemPoint);
+    } //else, clear the screen
     return;
 
 }
 
+/***************************Popup Windows****************************************************/
+static const char* items[]{ "Jill","Scott","Tanner" };
+static int selectedItem = 0;
+
+//Welcome Screen
+void showWelcomePopup(bool* p_open)
+{
+    //The window gets created here. Passing the bool to ImGui::Begin causes the "x" button to show in the top right of the window. Pressing the "x" button toggles the bool passed to it as "true" or "false"
+    //If the window cannot get created, it will call ImGui::End
+    if (!ImGui::Begin("Welcome to the revenue recovery app!", p_open))
+    {
+        ImGui::End();
+    }
+    else
+    {
+        ImGui::Text("Welcome to the revenue recovery app!");
+        ImGui::Text("Who are you?");
+        ImGui::ListBox("Chose your user", &selectedItem,
+            items, IM_ARRAYSIZE(items));
+        ImGui::End();
+
+    }
+}
+
+bool revAlert = false;
+void showMenuOptionsPopup(bool* p_open) {
+    //The window gets created here. Passing the bool to ImGui::Begin causes the "x" button to show in the top right of the window. Pressing the "x" button toggles the bool passed to it as "true" or "false"
+    //If the window cannot get created, it will call ImGui::End
+    if (!ImGui::Begin("What would you like to do?"))
+    {
+        ImGui::End();
+    }
+    else
+    {
+        ImGui::Text("What would you like to do?");
+        ImGui::Checkbox("View customer history?", &revAlert);
+        ImGui::End();
+
+    }
+}
+
+
 int main(int argc, char* argv[])
 {
     GLFWwindow* window;
+
+    //MultipleUsers userList;//each user list currently has 6 users
 
     testing::InitGoogleTest(&argc, argv);
 
@@ -131,17 +257,8 @@ int main(int argc, char* argv[])
         return -1;
     }
 
-
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
-
-    float vertices[] =
-    {
-        0 ,300, //top left
-        300, 300, //top right
-        300, 0, //bottom right
-        0, 0 // bottom left
-    };
 
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
@@ -149,68 +266,46 @@ int main(int argc, char* argv[])
     ImGui::StyleColorsDark();
     ImGui_ImplGlfw_InitForOpenGL(window, true);
     ImGui_ImplOpenGL3_Init("#version 330");
-
-    bool revAlert = false;
-
-    static const char* items[]{ "Jill","Scott","Tanner" };
-    static int selectedItem = 0;
-
+    
     glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT); //specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
     glMatrixMode(GL_PROJECTION);// projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ration and the near and far clipping planes
     glLoadIdentity(); // replace the current matrix with the identity matrix and starts us a fresh becausematrix transformations such as glOrpho and glRotate cumulate, basically puts us at (0,0,0)
-    glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1); //essentially set coordinate system
+    //glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1); //essentially set coordinate system
     glMatrixMode(GL_MODELVIEW); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
     glLoadIdentity(); //same as above comment
-
-    GLfloat pointVertex[] = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
-    GLfloat pointVertex2[] = { SCREEN_WIDTH * 0.75, SCREEN_HEIGHT * 0.75};
+    
+    static bool show_welcome_popup = true, 
+                show_menu_options_popup = true;
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-
-        // Render OpenGL here
-        glEnable(GL_POINT_SMOOTH);
-        glEnableClientState(GL_VERTEX_ARRAY);
-        glPointSize(50);//anywhere before drawarrays
-        glVertexPointer(2, GL_FLOAT, 0, pointVertex);
-        glDrawArrays(GL_POINTS, 0, 1);
-        glDisableClientState(GL_VERTEX_ARRAY);
-        glDisable(GL_POINT_SMOOTH);
-
-        glEnableClientState(GL_VERTEX_ARRAY);
-        glPointSize(10);//anywhere before drawarrays
-        glVertexPointer(2, GL_FLOAT, 0, pointVertex2);
-        glDrawArrays(GL_POINTS, 0, 1);
-        glDisableClientState(GL_VERTEX_ARRAY);
-
-        
+                
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplGlfw_NewFrame();
 
         if (selectedItem != 3) {
             ImGui::NewFrame();
-            ImGui::Begin("Welcome to the revenue recovery app!");
-            ImGui::Text("Who are you?");
-            ImGui::ListBox("Chose your user", &selectedItem,
-                items, IM_ARRAYSIZE(items));
-            ImGui::End();
+            if (show_welcome_popup)
+            {
+                showWelcomePopup(&show_welcome_popup);
+            }
         }
 
-        if (selectedItem == 1) {
-            ImGui::Begin("What would you like to do?");
-            ImGui::Checkbox("View customer history?", &revAlert);
-            ImGui::End();
+        if (selectedItem == 1) {            
+            show_welcome_popup = false;
+            if (show_menu_options_popup)
+            {
+                showMenuOptionsPopup(&show_menu_options_popup);
+            }
         }
         
-
         ImGui::Render();
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());      
  
-        
-        createGraph(selectedItem==1, revAlert);        
+        createGraph(selectedItem==1, revAlert);     
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
@@ -224,7 +319,7 @@ int main(int argc, char* argv[])
     ImGui::DestroyContext();
 
     glfwTerminate();
-
+    RUN_ALL_TESTS();
     return 0;
 }
 
--- ClosedGL/UserInfo.h
+++ ClosedGL/UserInfo.h
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include <graphicalUser.h>
 
 using namespace std;
 
@@ -38,8 +39,24 @@ public:
     int totalAmountPaid;
     int totalAmountExpected;
     int lengthOfMembership;
+    
+
+    float* expectedPaymentsToPlot;
+    float* actualPaymentsToPlot;
+    int* paymentLengthToPlot;
+
+    GLfloat** expectedPaymentGraphPoints;
+    GLfloat** actualPaymentGraphPoints;
+
     UserInfo(string name, int* expectedPayments, int* actualPayments, int lengthOfMembership, bool authentication);
     UserInfo();
+   
+    void setGraphCeiling(int ceiling);
+
+private:
+    void setLargestPayment();
+    int largestExpectedPayment;
+    int graphCeiling;
 };
 
 //Initial empty constructor
@@ -55,7 +72,49 @@ UserInfo::UserInfo(string name, int* expectedPayments, int* actualPayments, int 
     this->lengthOfMembership = lengthOfMembership;
     totalAmountExpected = 0;
     totalAmountPaid = 0;
+    largestExpectedPayment = 0;
+    expectedPaymentsToPlot = new float [lengthOfMembership];
+    actualPaymentsToPlot = new float[lengthOfMembership];
+    const int membLength = lengthOfMembership;
 
+    /***************Translate Graphical Information******************/
+    setLargestPayment();
+    cout <<"Largest Expected Payment:" << largestExpectedPayment << "\n";
+    setGraphCeiling(largestExpectedPayment);
+    cout<<"Graph Ceiling:"<< graphCeiling << "\n";
+    //Payments
+        for (int j = 0; j < lengthOfMembership; j++) {
+            expectedPaymentsToPlot[j] = START_Y + ((float)expectedPayments[j] / (float) largestExpectedPayment) * (END_Y - START_Y);
+            //cout << "Expected Payments:" << expectedPaymentsToPlot[j] << "\n";
+            actualPaymentsToPlot[j] = START_Y + ((float) actualPayments[j] / (float) largestExpectedPayment) * (END_Y - START_Y);
+            //cout << "Actual Payments:" << actualPaymentsToPlot[j] << "\n";
+        }        
+    //Dates
+    //Graphics
+        //verify the output of this
+        expectedPaymentGraphPoints = new GLfloat * [lengthOfMembership];
+        for (int i = 0; i < lengthOfMembership; i++) {
+            expectedPaymentGraphPoints[i] = new GLfloat[3];
+            expectedPaymentGraphPoints[i][0] =  START_X+ ((i/lengthOfMembership)*(END_X - START_X)); //possible divide by zero error
+            expectedPaymentGraphPoints[i][1] = expectedPaymentsToPlot[i];
+            //cout << "Expected Payments:" << expectedPaymentGraphPoints[i][1] << "\n";
+            expectedPaymentGraphPoints[i][2]= 0;
+ 
+        }
+
+        //print (x,y) values
+        actualPaymentGraphPoints = new GLfloat * [lengthOfMembership];
+        for (int i = 0; i < lengthOfMembership; i++) {
+            actualPaymentGraphPoints[i] = new GLfloat[3];
+            actualPaymentGraphPoints[i][0] = START_X + (((GLfloat)i / (float) lengthOfMembership) * (END_X - START_X)); //possible divide by zero error
+            actualPaymentGraphPoints[i][1] = actualPaymentsToPlot[i];
+            //cout << "Actual Payments:" << actualPaymentGraphPoints[i][1] << "\n";
+            actualPaymentGraphPoints[i][2] = 0;
+
+        }
+    /****************End Translate Graphical Information*************/
+
+    /*******Check Authentication********************************/
     if (authentication) {
         //Calculate the number of missed payments
         underpaymentCount = 0;
@@ -92,11 +151,31 @@ UserInfo::UserInfo(string name, int* expectedPayments, int* actualPayments, int 
     else
         status = "Authentication failed!";
     //cout << status;
+    /*********************************************************End Check Authentication************************************/
     return;
     //test cases, varying lengths of payment histories
     //consider an enum of the reasons a payment is delinquent
 }
 
+
+void UserInfo::setLargestPayment() {
+    for (int j = 0; j < lengthOfMembership; j++) {
+        if (expectedPayments[j] > largestExpectedPayment) {
+            //graph x axis based on largest payment
+            largestExpectedPayment = expectedPayments[j];
+        }
+    }
+    return;
+}
+
+//The user will pick a dollar amount to set the graph ceiling
+void UserInfo::setGraphCeiling(int ceiling) {
+    //perform calculation, then do comparison
+    int translatedGraphCeiling;
+    if (ceiling >= largestExpectedPayment)
+        graphCeiling = ceiling; //else, return an error
+        return;
+}
 //Create multiple users, useful for testing multiple scenarios
 class MultipleUsers {
 public:
@@ -110,5 +189,4 @@ public:
         }
         return;
     }
-
 };
